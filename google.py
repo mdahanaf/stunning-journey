77b25953ca0b47a8935b9966997415aa5f1e7b04
Md. Ahanaf <iammdahanaf@gmail.com>
2022-11-01 10:13:41 +0600

    Add done

---
+++ google.py
@@ -0,0 +1,10 @@
+import math
+
+num = float(input("Enter your number: "))
+
+result = math.sqrt(num)
+
+print("\n\nSquare root of ", num, " is: ", result, "\n\n")
+
+
+gfubv = input("Hit enter to exit....")
